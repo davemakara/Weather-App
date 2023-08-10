6b8fe03738949eb7f8c1cb863adc9f01d88ccf73
davidmakarovv <davidmakarr@gmail.com>
2023-08-11 01:32:18 +0400

    functionality added

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 "use strict";
 
 const apiKey = "86f9d2ad3489c07886b1f6ed970f748d";
+const btnSubmit = document.querySelector("#submitBtn");
+const inputSearch = document.querySelector("#search");
+const cityCard = document.querySelector(".cityInfo");
+// const citySearch = inputSearch.value.toLowerCase();
+const weatherPicture = document.querySelector("#weatherPic");
+const temperature = document.querySelector("#temperature");
+const cityName = document.querySelector("#city");
+const humidityPercentage = document.querySelector("#humidityPerc");
+const windPercentage = document.querySelector("#windPerc");
 
 const checkWeather = async function (city) {
   const response = await fetch(
@@ -8,7 +17,24 @@ const checkWeather = async function (city) {
   );
   const data = await response.json();
 
+  let weatherInfo = data.weather[0].main.toLowerCase();
+  weatherPicture.src = `./img/${weatherInfo}.png`;
+
+  temperature.innerHTML = Math.round(data.main.temp) + "°C";
+  cityName.innerHTML = data.name;
+  humidityPercentage.innerHTML = data.main.humidity + "%";
+  windPercentage.innerHTML = data.wind.speed + "km/h";
+
   console.log(data);
 };
-checkWeather("tbilisi");
-checkWeather("naples");
+
+btnSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let citySearch = inputSearch.value.toLowerCase();
+
+  checkWeather(citySearch);
+  cityCard.style.opacity = 1;
+
+  inputSearch.value = "";
+});
